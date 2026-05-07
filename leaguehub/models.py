8cbc5fc5d26b82f97b0486d86af79882c2aea6f4
tomlafferty <tsl5046@gmail.com>
2026-05-07 00:10:26 -0400

    commish settings and others

--- leaguehub/models.py
+++ leaguehub/models.py
@@ -34,6 +34,8 @@ class ManagerProfile(models.Model):
     )
     display_name = models.CharField(max_length=100)
     yahoo_guid = models.CharField(max_length=100, blank=True)
+    is_commissioner = models.BooleanField(default=False)
+    is_officer = models.BooleanField(default=False)
 
     def __str__(self):
         return self.display_name
